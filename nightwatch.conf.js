d95af117c7a1fe644bb60d79ebf161f8afa0b2e4
Dani Latorre <dani@danilat.com>
2017-11-06 18:56:43 +0100

    nightwatch configuration to use selenium-server and chromedriver

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,48 +1,27 @@
 require('nightwatch-cucumber')({});
 module.exports = {
   "output_folder" : "reports",
-  "custom_commands_path" : "",
-  "custom_assertions_path" : "",
-  "page_objects_path" : "",
-  "globals_path" : "",
 
   "selenium" : {
-    "start_process" : false,
-    "server_path" : "",
-    "log_path" : "",
+    "start_process" : true,
+    "server_path" : require('selenium-server').path,
     "port" : 4444,
     "cli_args" : {
-      "webdriver.chrome.driver" : "",
-      "webdriver.gecko.driver" : "",
-      "webdriver.edge.driver" : ""
+      "webdriver.chrome.driver" : require('chromedriver').path,
     }
   },
-
   "test_settings" : {
     "default" : {
-      "launch_url" : "http://localhost",
       "selenium_port"  : 4444,
       "selenium_host"  : "localhost",
       "silent": true,
-      "screenshots" : {
-        "enabled" : false,
-        "path" : ""
-      },
-      "desiredCapabilities": {
-        "browserName": "firefox",
-        "marionette": true
-      }
-    },
-
-    "chrome" : {
       "desiredCapabilities": {
         "browserName": "chrome"
       }
     },
-
-    "edge" : {
+    "chrome" : {
       "desiredCapabilities": {
-        "browserName": "MicrosoftEdge"
+        "browserName": "chrome"
       }
     }
   }
